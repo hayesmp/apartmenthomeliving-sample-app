58120db480783e8d3b0f4338dc52b399b32ece6d
Michael Hayes <tasslehawf@mac.com>
2013-11-15 03:42:41 -0600

    listings index updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ ApartmenthomelivingSampleApp::Application.routes.draw do
   root :to => 'listings#index'
 
   resources :listings do
+    collection do
+      get :search
+    end
     resources :floorplans
   end
 end
