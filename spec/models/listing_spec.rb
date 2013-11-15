4be894692f622e33f8530fe6b9354bb43a5661f0
Michael Hayes <tasslehawf@mac.com>
2013-11-14 23:33:38 -0600

    create listing_spec

---
+++ spec/models/listing_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+require 'json'
+
+describe Listing do
+  describe "retrieve a listing feed" do
+    feed = VCR.use_cassette('1br-apartment-feed') do
+      Listing.listing_json
+    end
+    it "returns a JSON string" do
+      expect(feed.class).to eq(String)
+      expect(JSON.is_json?(feed)).to eq(true)
+    end
+    it "parses the string and creates Listing records" do
+      Listing.parse_listing_feed(feed)
+      expect(Listing.all.count).to be > 0
+    end
+    it "creates valid Listing data" do
+      Listing.parse_listing_feed(feed)
+      listing = Listing.all.last
+      expect(listing).not_to eq nil
+      expect(listing.listing_id).not_to eq nil
+    end
+  end
+end
