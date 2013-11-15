328c3c5e87d5aedc12d6515e7e352dd5bb7fc4a9
Michael Hayes <tasslehawf@mac.com>
2013-11-15 03:42:04 -0600

    add show, search and other_listings to listings controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,8 +1,22 @@
 class ListingsController < ApplicationController
-  respond_to :html
+  respond_to :html, :json
+  before_filter :other_listings
+
+  def other_listings
+    @other_listings = Listing.order("updated_at DESC").take(6)
+  end
 
   def index
     @listings = Listing.all.take(10)
     respond_with @listings
+  end
+
+  def search
+
+  end
+
+  def show
+    @listing = Listing.find(params[:id])
+    respond_with @listing
   end
 end
