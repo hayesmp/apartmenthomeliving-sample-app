39fd06d3bdc0f61ec1818cf7a1d32234709d5ab7
Michael Hayes <tasslehawf@mac.com>
2013-11-15 03:41:23 -0600

    listings#show spec added

--- spec/controllers/listings_controller_spec.rb
+++ spec/controllers/listings_controller_spec.rb
@@ -17,10 +17,32 @@ describe ListingsController do
     end
 
     it "loads 10 or less listings into @listings" do
-
       get :index
 
       expect(assigns(:listings).count).to be <= 10
+    end
+  end
+
+  describe "GET #show" do
+    before(:each) do
+      Listing.parse_listing_feed(VCR.use_cassette('1br-apartment-feed') {Listing.listing_json})
+      @listing = Listing.first
+    end
+    it "responds successfully with an HTTP 200 status code" do
+      get :show, id: @listing.id
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+
+    it "renders the show template" do
+      get :show, id: @listing.id
+      expect(response).to render_template("show")
+    end
+
+    it "loads a listing" do
+      get :show, id: @listing.id
+
+      expect(assigns(:listing).pricing).not_to be nil
     end
   end
 end
